76d296866b0b4e88b5878782bdfa0ec230a27bd0
Kseniya Kovalenko <kkovalenko051@gmail.com>
2025-01-19 23:07:13 +0300

    Changes

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,18 +1,20 @@
 import pytest
 from praktikum.bun import Bun
-@pytest.mark.parametrize("name, price", [
+
+class TestBun:
+    @pytest.mark.parametrize("name, price", [
     ("Лучшая", 9.9),
     ("Сырная", 1.3)
-])
-def test_configuration_bun(name, price):
-    bun = Bun(name, price)
-    assert bun.get_name() == name
-    assert bun.get_price() == price
+     ])
+    def test_configuration_bun(name, price):
+        bun = Bun(name, price)
+        assert bun.get_name() == name
+        assert bun.get_price() == price
 
-def test_get_name_bun():
-    bun = Bun("Пышная", 1.1)
-    assert bun.get_name() == "Пышная"
+    def test_get_name_bun(self):
+        bun = Bun("Пышная", 1.1)
+        assert bun.get_name() == "Пышная"
 
-def test_get_price_bun():
-    bun = Bun("Мягкая", 1.9)
-    assert bun.get_price() == 1.9
+    def test_get_price_bun(self):
+        bun = Bun("Мягкая", 1.9)
+        assert bun.get_price() == 1.9
